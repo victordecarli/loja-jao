0b981b14bdecdcfb8ab50f1920abcc52dfeba93c
V0nor <=>
2025-03-03 22:07:17 -0400

    [FEAT] add sistema de temas de interface no app.

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,13 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
+import { ThemeProvider } from '@/themes/themeContext.tsx';
 import App from './App/index.tsx';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <ThemeProvider>
+      <App />
+    </ThemeProvider>
   </StrictMode>,
 );
---
+++ frontend/src/themes/themeContext.tsx
@@ -0,0 +1,31 @@
+// src/theme/ThemeContext.tsx
+import React, { createContext, useContext, useEffect, useState } from 'react';
+
+export type Theme = 'default' | 'lol' | 'stardewValley';
+
+interface ThemeContextProps {
+    theme: Theme;
+    setTheme: (theme: Theme) => void;
+}
+
+const ThemeContext = createContext<ThemeContextProps>({
+    theme: 'default',
+    setTheme: () => { },
+});
+
+export const useTheme = () => useContext(ThemeContext);
+
+export const ThemeProvider: React.FC<{ children: React.ReactNode }> = ({ children }) => {
+    const [theme, setTheme] = useState<Theme>('default');
+
+    useEffect(() => {
+        // Define o atributo "data-theme" no elemento raiz para aplicar os estilos do tema
+        document.documentElement.setAttribute('data-theme', theme);
+    }, [theme]);
+
+    return (
+        <ThemeContext.Provider value={{ theme, setTheme }}>
+            {children}
+        </ThemeContext.Provider>
+    );
+};
