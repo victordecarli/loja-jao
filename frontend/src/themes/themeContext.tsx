cf16b905620c75d3966b50886873ff5a39b6bafb
victordecarli <vonorvictor@gmail.com>
2025-03-04 12:55:16 -0400

    [FEAT] Adiciona temas utilizando o useContext

--- frontend/src/themes/themeContext.tsx
+++ frontend/src/themes/themeContext.tsx
@@ -4,28 +4,24 @@ import React, { createContext, useContext, useEffect, useState } from 'react';
 export type Theme = 'default' | 'lol' | 'stardewValley';
 
 interface ThemeContextProps {
-    theme: Theme;
-    setTheme: (theme: Theme) => void;
+  theme: Theme;
+  setTheme: (theme: Theme) => void;
 }
 
 const ThemeContext = createContext<ThemeContextProps>({
-    theme: 'default',
-    setTheme: () => { },
+  theme: 'default',
+  setTheme: () => {}
 });
 
 export const useTheme = () => useContext(ThemeContext);
 
 export const ThemeProvider: React.FC<{ children: React.ReactNode }> = ({ children }) => {
-    const [theme, setTheme] = useState<Theme>('default');
+  const [theme, setTheme] = useState<Theme>('default');
 
-    useEffect(() => {
-        // Define o atributo "data-theme" no elemento raiz para aplicar os estilos do tema
-        document.documentElement.setAttribute('data-theme', theme);
-    }, [theme]);
+  useEffect(() => {
+    // Define o atributo "data-theme" no elemento raiz para aplicar os estilos do tema
+    document.documentElement.setAttribute('data-theme', theme);
+  }, [theme]);
 
-    return (
-        <ThemeContext.Provider value={{ theme, setTheme }}>
-            {children}
-        </ThemeContext.Provider>
-    );
+  return <ThemeContext.Provider value={{ theme, setTheme }}>{children}</ThemeContext.Provider>;
 };
