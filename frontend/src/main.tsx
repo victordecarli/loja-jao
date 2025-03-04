0182a772e32a7012e8044b1d8136802003551f90
victordecarli <vonorvictor@gmail.com>
2025-03-04 18:59:12 -0400

    [FEAT] Simplifica o main.tsx

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,13 +1,13 @@
-import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import { ThemeProvider } from '@/themes/themeContext.tsx';
 import App from './App/index.tsx';
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <ThemeProvider>
-      <App />
-    </ThemeProvider>
-  </StrictMode>
-);
+const root = createRoot(document.getElementById('root'));
+
+root.render(
+  <ThemeProvider>
+    <App />
+  </ThemeProvider>
+)
+
