8c1369a699ed27d5862f35d9da6fa6127da8ca59
victordecarli <vonorvictor@gmail.com>
2025-03-05 11:38:49 -0400

    [FEAT] Configura 0auth no frontend

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,13 +1,28 @@
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import { ThemeProvider } from '@/themes/themeContext.tsx';
-import App from './App/index.tsx';
+import { Auth0Provider } from '@auth0/auth0-react';
+import App from '@/App';
+
+const domain = process.env.REACT_APP_AUTH0_DOMAIN!;
+const clientId = process.env.REACT_APP_AUTH0_CLIENT_ID!;
+// Caso você precise enviar uma audiência para o backend, inclua:
+const audience = process.env.REACT_APP_AUTH0_AUDIENCE;
 
 const root = createRoot(document.getElementById('root'));
 
 root.render(
   <ThemeProvider>
-    <App />
+    <Auth0Provider
+      domain={domain}
+      clientId={clientId}
+      authorizationParams={{
+        redirect_uri: window.location.origin,
+        audience,
+      }}
+    >
+      <App />
+    </Auth0Provider>
   </ThemeProvider>
 )
 
